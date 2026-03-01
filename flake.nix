5c79d0f41414a430ef05305d70ea49b5a5a99841
Paul Chobert <paul@chobert.fr>
2026-03-01 19:52:14 +0100

    Add Nix flake dev shell and GitHub Actions release workflow
    Nix flake provides Rust toolchain and OpenSSL for reproducible builds.
    Release workflow triggers on v* tags and builds native binaries for
    x86_64/aarch64 on Linux and macOS.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.openssl
+              pkgs.pkg-config
+            ];
+          };
+        });
+    };
+}
