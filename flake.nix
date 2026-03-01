951a17b0e5cb77df2b6a2705c24f978a43431e49
Paul Chobert <paul@chobert.fr>
2026-03-01 20:53:44 +0100

    Build static musl binaries via nix build
    Replaces dynamic glibc-linked builds with fully static
    musl binaries for portability. Adds nix package definition
    using pkgsStatic.rustPlatform.buildRustPackage.

--- flake.nix
+++ flake.nix
@@ -6,8 +6,24 @@
   outputs = { nixpkgs, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      linuxSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
     in
     {
+      packages = linuxSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+            pname = "zfs-cloud-backup";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.pkgsStatic.openssl ];
+          };
+        });
+
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
